562d033e01d9bbef3750e4b51e92b137a2cdb0b6
Alexander Tranvaag <awt49@cornell.edu>
2021-12-01 21:50:36 -0500

    feat: add login api interface

--- model-interfaces/IUser.ts
+++ model-interfaces/IUser.ts
@@ -1,21 +1,24 @@
 export class IUser {
     id: string;
-    email: string;
+    netId: string;
     firstName: string;
     lastName: string;
+    termsAccepted: boolean;
     permissions: number;
 
     constructor(
         id: string,
-        email: string,
+        netId: string,
         firstName: string,
         lastName: string,
-        permissions: number
+        termsAccepted: boolean,
+        permissions: number,
     ) {
         this.id = id;
-        this.email = email;
+        this.netId = netId;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.termsAccepted = termsAccepted;
         this.permissions = permissions;
     }
 }
